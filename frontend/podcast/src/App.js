9a6f85eb660cd7721b2736f35488487a890dd0e0
fahdbahri <fahdkhalid2002@gmail.com>
2024-12-18 07:55:11 +0200

    feat: initialize projec folder including react set up

---
+++ frontend/podcast/src/App.js
@@ -0,0 +1,26 @@
+import React, { useState, useEffect } from "react";
+
+function App() {
+  const [data, setData] = useState([{}]);
+
+  useEffect(() => {
+    fetch("/home")
+      .then((res) => res.json())
+      .then((data) => {
+
+        setData(data);
+        console.log(data);
+      });
+  }, []);
+  return (
+    <div>
+      {(typeof data.Name === 'undefined') ? (
+        <p>Loading...</p>
+      ) : (
+        data.Name.map((member, i) => <p key={i}>{member}</p>)
+      )}
+    </div>
+  );
+}
+
+export default App;
