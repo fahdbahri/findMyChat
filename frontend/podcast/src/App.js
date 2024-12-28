757713607d2df739c93bb5d5fc5f2cb47707c540
fahdbahri <fahdkhalid2002@gmail.com>
2024-12-28 08:29:21 +0200

    fix(delete): delete some uncessary files cleaning the code

--- frontend/podcast/src/App.js
+++ frontend/podcast/src/App.js
@@ -1,30 +1,20 @@
-import React, { useState, useEffect } from "react";
-import TextField from "@mui/material/TextField"
+import React, { useState } from "react";
+import "./App.css";
+import VideoList from "./components/videoslist";
+import SearchInput from "./components/SearchInput";
 
 
 function App() {
-  const [data, setData] = useState([{}]);
+  const [videos, setVideos] = useState([]);
 
-  useEffect(() => {
-    fetch("/home")
-      .then((res) => res.json())
-      .then((data) => {
+  const showResults = (newVideos) => {
+    setVideos(newVideos);
+  };
 
-        setData(data);
-        console.log(data);
-      });
-  }, []);
   return (
-    <div className="main">
-      <h1>Search Your Favourite Podcast</h1>
-      <div className="search">
-        <TextField 
-        id="outlined-basic"
-        variant="outlined"
-        fullWidth
-        label="Seach"
-        />
-      </div>
+    <div>
+      <SearchInput onSearch={showResults} />
+      <VideoList videos={videos} />
     </div>
   );
 }
